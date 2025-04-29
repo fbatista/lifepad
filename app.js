ae2af7a3bb1f5a7411b55a68b921c902d2a5e1bb
Fábio Batista <fbatista@gmail.com>
2025-04-29 19:19:11 +0100

    Icons

---
+++ app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  Array.prototype.forEach.call(
+    document.getElementsByClassName("input"),
+    (input) => {
+      input.timeout = null;
+      var current = input.getElementsByClassName("current")[0];
+      var clickHandler = function (operation) {
+        return function (event) {
+          current.value = parseInt(current.value) + operation;
+          clearTimeout(input.timeout);
+          input.timeout = setTimeout(() => {
+            var newEntry = document.createElement("li");
+            newEntry.innerText = current.value;
+            input.parentElement
+              .getElementsByClassName("history")[0]
+              .append(newEntry);
+            newEntry.scrollIntoView({
+              behavior: "smooth",
+              block: "end",
+              inline: "nearest",
+            });
+          }, 750);
+        };
+      };
+
+      input.getElementsByClassName("up")[0].onclick = clickHandler(1);
+      input.getElementsByClassName("down")[0].onclick = clickHandler(-1);
+    },
+  );
+});
